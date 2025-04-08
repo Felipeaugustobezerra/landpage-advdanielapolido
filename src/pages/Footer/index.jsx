f3ccd4fe545f897a40ddceef3c21889156562148
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2025-04-08 20:21:20 +0100

    updates icons social medias

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import { FaInstagram } from "react-icons/fa6";
 import { IoLogoFacebook } from "react-icons/io";
+import { IoLogoLinkedin } from "react-icons/io5";
 import Logo from "../../assets/Logo/Logo.png";
 import { Link } from "react-router-dom";
 
@@ -20,11 +21,19 @@ const Footer = () => {
             </span>
           </Link>
           <Link
-            to="https://www.instagram.com/adv.danielapolido/"
+            to="https://www.facebook.com/profile.php?id=61556139986938"
             target="_blank"
           >
             <span className="facebook">
               <IoLogoFacebook />
+            </span>
+          </Link>
+          <Link
+            to="https://www.linkedin.com/in/daniela-polido-7773b1a3/"
+            target="_blank"
+          >
+            <span className="Linkedin">
+              <IoLogoLinkedin />
             </span>
           </Link>
         </div>
